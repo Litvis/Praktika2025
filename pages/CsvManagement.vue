9144e6370ecbb0ad53b9377b8349ba096f982eb9
Litvis <deividaslitvinenko4@gmail.com>
2025-04-15 16:24:58 +0300

    csv

---
+++ pages/CsvManagement.vue
@@ -0,0 +1,9 @@
+<template>
+    <Sidebar />
+    <CsvManagement />
+  </template>
+  
+  <script setup>
+  import Sidebar from '~/components/adminlanding/Sidebar.vue';
+  import CsvManagement from '~/components/adminlanding/CsvManagement.vue';
+  </script>
