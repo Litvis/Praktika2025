476644fa7e2ea94d382708f17c50241b13f5d48f
Litvis <deividaslitvinenko4@gmail.com>
2025-04-15 12:43:58 +0300

    :D

--- pages/UserManagement.vue
+++ pages/UserManagement.vue
@@ -1,7 +1,9 @@
 <template>
+    <Sidebar />
     <usersManagement />
   </template>
   
   <script setup>
+  import Sidebar from '~/components/adminlanding/Sidebar.vue';
   import usersManagement from '~/components/adminlanding/usersManagement.vue';
   </script>
